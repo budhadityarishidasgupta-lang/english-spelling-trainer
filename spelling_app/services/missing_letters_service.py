1911390a20fe6180b88246304678a0bf6dac549b
budhadityarishidasgupta-lang <budhaditya.rishi.dasgupta@gmail.com>
2025-12-04 13:15:55 +0000

    Cleanup: removed legacy spelling files, retaining spelling_clean_app.py

--- spelling_app/services/missing_letters_service.py
+++
@@ -1,8 +0,0 @@
-from spelling_app.repository.missing_letters_repo import fetch_missing_letter_words
-
-
-def get_missing_letter_words():
-    """
-    Wrapper for UI consumption.
-    """
-    return fetch_missing_letter_words()
--- spelling_app/services/weak_words_service.py
+++
@@ -1,7 +0,0 @@
-from spelling_app.repository.weak_words_repo import get_weak_words_summary
-
-def load_weak_words():
-    """
-    Wrapper for UI consumption.
-    """
-    return get_weak_words_summary()
