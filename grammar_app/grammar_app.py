2ba593c4f3bf0e4f25af323d560f8a216fca47da
RishiBazz <budhaditya.rishi.dasgupta@gmail.com>
2026-06-03 14:08:00 +0200

    Add GrammarSprint package entrypoint

---
+++ grammar_app/grammar_app.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from grammar_app.admin_ui import render_grammar_admin
+from grammar_app.student_ui import render_grammar_lesson_list, render_grammar_practice, render_grammar_student
+
+__all__ = [
+    "render_grammar_admin",
+    "render_grammar_lesson_list",
+    "render_grammar_practice",
+    "render_grammar_student",
+]
