17aee7e7380fb152a505aeab5d407eef011ccfbe
RishiBazz <budhaditya.rishi.dasgupta@gmail.com>
2026-06-03 14:12:20 +0200

    Make GrammarSprint wrapper self-contained

--- grammar_app/grammar_app.py
+++ grammar_app/grammar_app.py
@@ -1,11 +1,17 @@
 from __future__ import annotations
 
-from grammar_app.admin_ui import render_grammar_admin
-from grammar_app.student_ui import render_grammar_lesson_list, render_grammar_practice, render_grammar_student
+import streamlit as st
 
-__all__ = [
-    "render_grammar_admin",
-    "render_grammar_lesson_list",
-    "render_grammar_practice",
-    "render_grammar_student",
-]
+from grammar_app.admin_ui import render_grammar_admin
+from grammar_app.student_ui import GRAMMAR_PAGE_KEY, render_grammar_lesson_list, render_grammar_practice
+
+
+def render_grammar_student() -> None:
+    page = st.session_state.get(GRAMMAR_PAGE_KEY, "lesson_list")
+    if page == "practice":
+        render_grammar_practice()
+        return
+    render_grammar_lesson_list()
+
+
+__all__ = ["render_grammar_admin", "render_grammar_student"]
